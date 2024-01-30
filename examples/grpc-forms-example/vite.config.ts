78b1ba8c3f5117cfb6ffb98f98db20a2f45549a3
Tyler Vick <tyler@tylervick.com>
2024-01-30 16:33:15 -0700

    merge example vite configs

--- examples/grpc-forms-example/vite.config.ts
+++ examples/grpc-forms-example/vite.config.ts
@@ -2,21 +2,22 @@ import react from '@vitejs/plugin-react';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { defineConfig } from 'vitest/config';
+import { defineConfig, mergeConfig } from 'vitest/config';
 
 const path = fileURLToPath(import.meta.url);
-export const root = resolve(dirname(path));
-export const src = resolve(root, 'src');
+const root = resolve(dirname(path), 'src');
 
-export default defineConfig({
-  root: src,
-  plugins: [react(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      '@': resolve(dirname(path), '..', '..', 'src'),
+export default defineConfig((envConfig) =>
+  mergeConfig(envConfig, {
+    root,
+    plugins: [react(), tsconfigPaths()],
+    resolve: {
+      alias: {
+        '@': resolve(dirname(path), '..', '..', 'src'),
+      },
     },
-  },
-  build: {
-    target: 'ESNext',
-  },
-});
+    build: {
+      target: 'ESNext',
+    },
+  }),
+);
